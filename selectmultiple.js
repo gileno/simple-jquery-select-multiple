dae4ef78a91a0cecbeb6ccd0c1acc6edb251c73a
Gileno Filho <gascf.cin@gmail.com>
2011-06-20 21:11:05 -0300

    checkbox click event added

--- selectmultiple.js
+++ selectmultiple.js
@@ -1,22 +1,45 @@
 (function($) {
     $.fn.selectMultiple = function(options) {
-        var defaults = {};
+        var defaults = {
+            containers: 1
+        };
         var settings = $.extend(defaults, options);
-        this.each(function(x) {
+        return this.each(function(x) {
             var select = $(this);
             var selectOptions = select.find("option");
             var container = $("<div />", {
                 "class": 'select-multiple-container'
             });
+            var column = $("<div />", {
+                "class": 'select-multiple-column'
+            });
+            var containers = settings.containers;
+            var totalOpts = selectOptions.length;
+            var remainder = totalOpts % containers;
+            var by_columns = (totalOpts - remainder) / containers;
+            if(remainder > 0) {
+                by_columns = by_columns + 1;
+            }
+            var counter = 0;
             selectOptions.each(function(y) {
                 var opt = $(this);
                 var value = opt.val();
-                var text = opt.html();
+                var text = opt.text();
+                var selected = true ? opt.is(':selected') : false;
                 var id = 'select-multiple-opt-'+ x + '-' + y; 
                 var input = $("<input />", {
                     type: 'checkbox',
                     value: value,
-                    id: id
+                    id: id,
+                    checked: selected
+                });
+                input.change(function(e) {
+                    var select_opt = select.find("option[value=" + $(this).val() + "]");
+                    if($(this).is(':checked')) {
+                        select_opt.attr("selected", "selected");
+                    } else {
+                        select_opt.attr("selected", "");
+                    }
                 });
                 var label = $("<label />", {
                     'for': id,
@@ -27,10 +50,23 @@
                 });
                 optContainer.append(input);
                 optContainer.append(label);
-                container.append(optContainer);
+                if(counter < by_columns) {
+                    counter = counter + 1;
+                } else {
+                    container.append(column);
+                    column = $("<div />", {
+                        "class": 'select-multiple-column'
+                    });
+                    counter = 1;   
+                }
+                column.append(optContainer);
+                if((y + 1) == totalOpts) {
+                    container.append(column);
+                }
             });
             select.after(container);
             select.hide();            
         });
     }
 })(jQuery);
+
